99cba9f379401e871a8da8ddfd09d81f3dc8db84
adriaanmolendijk <118157982+adriaanmolendijk@users.noreply.github.com>
2026-06-29 18:29:39 +0200

    Create problem.go

---
+++ problems/021/problem.go
@@ -0,0 +1,25 @@
+package main
+
+import "fmt"
+
+func main() {
+	sum := 0
+	for i := 1; i <= 10_000; i++ {
+		divs := sumDivisors(i)
+		if divs != i && sumDivisors(divs) == i {
+			fmt.Println(i)
+			sum += i
+		}
+	}
+	fmt.Println(sum)
+}
+
+func sumDivisors(n int) int {
+	sum := 0
+	for i := 1; i < n; i++ {
+		if n%i == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
